b44402aa636079b37fed4c2b1b6895746528e94c
diganthrai <diganthrai>
2024-08-04 07:34:53 +0000

    commit secret

---
+++ secrets methods mount (1).py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+dbutils.fs.mount(
+    source = "wasbs://wafacontainer@wafastoragede.blob.core.windows.net/",   
+    mount_point = "/mnt/data",
+    extra_configs = {"<conf-key>":dbutils.secrets.get(scope = "<scope-name>", key = "<key-name>")}
+
+)
+
+# COMMAND ----------
+
+
